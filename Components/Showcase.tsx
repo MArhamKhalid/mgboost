f4c839be18e228152f769b2e777cee2063078abe
Arham khalid <arhamkhalid79@gmail.com>
2026-08-13 02:59:31 +0500

    changing animation

--- Components/Showcase.tsx
+++ Components/Showcase.tsx
@@ -36,7 +36,79 @@ const Showcase = () => {
 
     const sectionRef = useRef(null);
     useEffect(() => {
-  const ctx = gsap.context(() => {
+    const ctx = gsap.context(() => {
+
+        gsap.set(".scene-1", {
+            yPercent: 0,
+        });
+
+        gsap.set(".scene-2", {
+            yPercent: 100,
+        });
+
+        gsap.set(".scene-3", {
+            yPercent: 100,
+        });
+
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: sectionRef.current,
+                start: "top top",
+                end: "bottom bottom",
+                scrub: 1,
+                markers: true,
+            }
+        });
+
+        // --------------------------------
+        // SCENE 1
+        // --------------------------------
+
+        tl.to({}, { duration: 2.5 });
+
+        // Scene 1 → Scene 2
+        tl.to(".scene-1", {
+            // opacity: 0,
+            yPercent: -100,
+            duration: 2.5,
+            ease: 'none',
+        });
+
+        tl.to(".scene-2", {
+            // opacity: 1,
+            yPercent: 10,
+            duration: 2.5,
+            ease: 'none',
+        });
+
+        // --------------------------------
+        // SCENE 2
+        // --------------------------------
+
+        tl.to({}, { duration: 2.5 });
+
+        // Scene 2 → Scene 3
+        tl.to(".scene-2", {
+            // opacity: 0,
+            yPercent: -100,
+            duration: 2.5,
+            ease: 'none',
+        });
+
+        tl.to(".scene-3", {
+            // opacity: 1,
+            yPercent: 0,
+            duration: 2.5,
+            ease: 'none',
+        });
+
+        // --------------------------------
+        // SCENE 3
+        // --------------------------------
+
+        tl.to({}, { duration: 2.5 });
+
     gsap.fromTo(
       ".behindCantext",
       {
@@ -67,7 +139,7 @@ const Showcase = () => {
         {
             // opacity: 0,
             clipPath: "inset(0 0 0% 0)",
-            duration: 0.8,
+            duration: 0.5,
             ease: "power1.inOut",
             delay: 0.5,
 
@@ -90,7 +162,7 @@ const Showcase = () => {
             rotate: -8,
             duration: 0.8,
             ease: "power1" ,
-            delay: 1.2,
+            delay: 1.8,
 
             scrollTrigger: {
                 trigger: sectionRef.current,
@@ -112,7 +184,7 @@ const Showcase = () => {
             rotate: 8,
             duration: 0.8,
             ease: "power1" ,
-            delay: 1.2,
+            delay: 1.8,
 
             scrollTrigger: {
                 trigger: sectionRef.current,
@@ -135,7 +207,7 @@ const Showcase = () => {
             x: 0,
             duration: 0.6,
             ease: "power1.inOut",
-            delay: 1,
+            delay: 1.5,
 
             scrollTrigger: {
               trigger: sectionRef.current,
@@ -158,7 +230,7 @@ const Showcase = () => {
             x: 0,
             duration: 0.6,
             ease: "power1.inOut",
-            delay: 1,
+            delay: 1.5,
 
             scrollTrigger: {
               trigger: sectionRef.current,
@@ -200,14 +272,14 @@ const Showcase = () => {
 
     return (
         // <div className=" top-0 h-screen">
-        <section ref={sectionRef} className="relative w-full h-[300vh] bg-white  ">
-            <div className="sticky top-0 w-full flex flex-col gap-y-[32px] h-screen items-center">
-                <div className="absolute inset-0 z-10 teanima flex justify-center items-center ">
+        <section ref={sectionRef} className="relative w-full h-[200vh] bg-white  ">
+            <div className="sticky top-0 w-full  h-screen overflow-hidden">
+                <div className="scene scene-1 w-full h-screen absolute inset-0 z-10 teanima flex justify-center items-center  bg-white ">
                     <h2 className=" max-w-[984px]  max-h-[272px] capitalize font-Alan text-center text-[#818181] text-[44px] leading-[68px] font-light firsthead">
                         To make magnesium simple, <span className="text-[#DAA404] font-bold capitalize">enjoyable, and part of everyday life.Instead of complicated supplements</span>, we believe wellness should fit naturally into your routine
                     </h2>
                 </div>
-                <div className="absolute inset-0 z-20 px-15 flex gap-x-[32px] text-black py-15 ">
+                <div className="scene scene-2 w-full h-screen absolute inset-0 z-20 px-15 flex gap-x-[32px] text-black bg-white">
                     <div className="w-144.5 h-[931px] Can1  ">
                         <div className="w-full h-165 bg-[#FFF4D7] rounded-b-xl flex justify-center rounded-[16px]">
                             <img src="/images/mango-flavor.png" alt="" className="mt-14 w-67.5 h-176" />
@@ -263,47 +335,49 @@ const Showcase = () => {
                         </div>
                     </div>
                 </div>
-                <div className="absolute inset-0  z-30 w-full px-25 flex justify-center gap-x-[32px] text-black ">
-                    <div className="w-135 h-171 rounded-[20px] bg-gradient-to-b from-[#FFEFC0] to-[#F4B08C] relative">
-                        <img src="/images/can-holding-woman.png" alt="" className="w-full absolute bottom-0" />
-                    </div>
-                    <div className="w-294 h-171 rounded-[20px] bg-gradient-to-t from-[#FAF9BA] to-[#C3F8D8] relative">
-                        <div className="w-full absolute bottom-0">
-                            <img src="/images/ice.png" alt="" className="object-cover w-full rounded-b-[20px]" />
+                <div className="scene scene-3 absolute inset-0 z-30 w-full h-screen flex flex-col justify-end  bg-white">
+                    <div className="w-full flex justify-center px-25  py-20 gap-x-[32px] text-black">
+                        <div className="w-135 h-171 rounded-[20px] bg-gradient-to-b from-[#FFEFC0] to-[#F4B08C] relative">
+                            <img src="/images/can-holding-woman.png" alt="" className="w-full absolute bottom-0" />
                         </div>
-                        <div className="w-full absolute left-0 bottom-0">
-                            <img src="/images/double-lemon.png" alt="" className="" />
-                        </div>
-                        <div className="w-full absolute left-0 bottom-0 z-1">
-                            <img src="/images/single-lemon.png" alt="" className="" />
-                        </div>
-                        <div className="w-full absolute left-0 top-0">
-                            <img src="/images/mg-frame.png" alt="" className="" />
-                        </div>
-                        <div className="w-full flex justify-end ">
-                            <div className="w-140 h-full flex flex-col items-center text-center capitalize mt-[117px]">
-                                <div className="leading-[50px]">
-                                    <h2 className="font-Alan text-[82px] font-bold text-[#3F6C48] ">find your</h2>
-                                    <h2 className=" py-6 px-2 rounded-2xl bg-[#3F6C48] text-white text-[82px] font-bold -rotate-3">daily boost</h2>
+                        <div className="w-294 h-171 rounded-[20px] bg-gradient-to-t from-[#FAF9BA] to-[#C3F8D8] relative">
+                            <div className="w-full absolute bottom-0">
+                                <img src="/images/ice.png" alt="" className="object-cover w-full rounded-b-[20px]" />
+                            </div>
+                            <div className="w-full absolute left-0 bottom-0">
+                                <img src="/images/double-lemon.png" alt="" className="" />
+                            </div>
+                            <div className="w-full absolute left-0 bottom-0 z-1">
+                                <img src="/images/single-lemon.png" alt="" className="" />
+                            </div>
+                            <div className="w-full absolute left-0 top-0">
+                                <img src="/images/mg-frame.png" alt="" className="" />
+                            </div>
+                            <div className="w-full flex justify-end ">
+                                <div className="w-140 h-full flex flex-col items-center text-center capitalize mt-[117px]">
+                                    <div className="leading-[50px]">
+                                        <h2 className="font-Alan text-[82px] font-bold text-[#3F6C48] ">find your</h2>
+                                        <h2 className=" py-6 px-2 rounded-2xl bg-[#3F6C48] text-white text-[82px] font-bold -rotate-3">daily boost</h2>
+                                    </div>
+                                    <p className="text-[28px] font-Albert font-normal text-[#3F6C48] leading-[28px] mt-[37px]">Choose the perfect option<br />for your lifestyle.</p>
+                                    <button className="w-[158px] h-[42px] flex items-center cursor-pointer z-10 capitalize underline gap-x-4 mt-[29px]">
+                                        <p className="text-[20px] font-Albert">shop now </p>
+                                        <span className="w-[42px] h-[42px] bg-black rounded-full flex justify-center items-center">
+                                            <img src="/icons/Arrow-orange.svg" alt="" className='px-2 py-3' />
+                                        </span>
+                                    </button>
                                 </div>
-                                <p className="text-[28px] font-Albert font-normal text-[#3F6C48] leading-[28px] mt-[37px]">Choose the perfect option<br />for your lifestyle.</p>
-                                <button className="w-[158px] h-[42px] flex items-center cursor-pointer z-10 capitalize underline gap-x-4 mt-[29px]">
-                                    <p className="text-[20px] font-Albert">shop now </p>
-                                    <span className="w-[42px] h-[42px] bg-black rounded-full flex justify-center items-center">
-                                        <img src="/icons/Arrow-orange.svg" alt="" className='px-2 py-3' />
-                                    </span>
-                                </button>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className=" w-full h-[230px] bg-[#FFF7DF] flex justify-center items-end -mt-20  text-black">
-                    {textslider.map((text, i) => (
-                    <div className="w-full flex justify-center items-center text-slider mb-5 z-1 " key={i}>
-                        <span className="w-150 flex items-center text-center justify-evenly px-5 capitalize text-[40px] font-Albert font-light leading-[26px]"><p>{text.text}</p>
-                        <img src="/images/flower.png" alt=""/></span>
+                    <div className=" w-full h-[230px] bg-[#FFF7DF] flex justify-center items-end -mt-20  text-black">
+                        {textslider.map((text, i) => (
+                        <div className="w-full flex justify-center items-center text-slider mb-5 z-1 " key={i}>
+                            <span className="w-150 flex items-center text-center justify-evenly px-5 capitalize text-[40px] font-Albert font-light leading-[26px]"><p>{text.text}</p>
+                            <img src="/images/flower.png" alt=""/></span>
+                        </div>
+                        ))}
                     </div>
-                    ))}
                 </div>
                 
             </div>
